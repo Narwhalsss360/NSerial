c297fba035c137a3a37676376b698eae567b4cd4
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-05-14 17:12:36 -0400

    Fully implemented, testing required

--- src/NSerialCom.cpp
+++ src/NSerialCom.cpp
@@ -1,1 +1,138 @@
 #include "NSerialCom.h"
+
+NSerialData::NSerialData()
+    : address(NULL), data(NULL), length(NULL)
+{
+}
+
+NSerialData::NSerialData(uint16_t addr, void *pData, uint8_t len)
+    : address(addr), data((uint8_t *)malloc(length)), length(len)
+{
+    memcpy(data, pData, length);
+}
+
+NSerialData::NSerialData(uint16_t addr, String str)
+    : NSerialData(addr, (void *)str.c_str(), str.length() + 1)
+{
+}
+
+NSerialData::NSerialData(uint16_t addr, uint32_t num)
+    : NSerialData(addr, (void *)&num, sizeof(num))
+{
+}
+
+NSerialData::NSerialData(uint16_t addr, char *str)
+    : NSerialData(addr, (void *)str, strlen(str) + 1)
+{
+}
+
+void NSerialData::get(uint32_t &out)
+{
+    out = reinterpret_c_style(uint32_t, data);
+}
+
+void NSerialData::get(String &out)
+{
+    out = (char *)data;
+}
+
+NSerialComClass::NSerialComClass()
+    :data({ NSerialData() })
+{
+}
+
+uint8_t NSerialComClass::search(uint16_t addr)
+{
+    for (uint8_t i = ZERO; i < NSD_LENGTH; i++)
+    {
+        if (data[i].address == i)
+            return i;
+    }
+    return NSD_LENGTH;
+}
+
+void NSerialComClass::storeData(NSD newData)
+{
+    uint8_t index = search(newData.address);
+    if (index < NSD_LENGTH)
+    {
+        data[index] = newData;
+    }
+    else
+    {
+        if (nextAdd == NSD_LENGTH)
+            nextAdd = ZERO;
+        data[nextAdd] = newData;
+        nextAdd++;
+    }
+}
+
+void NSerialComClass::serialEvent()
+{
+    uint8_t count = ZERO;
+    bool end = false;
+
+    while (Serial.available())
+    {
+        if (count < IN_STREAM_BUFFER_LENGTH || !end)
+            inStreamBuffer[count] = (char)Serial.read();
+        else
+            Serial.read();
+        if (inStreamBuffer[count] == EOT)
+            end = true;
+        count++;
+    }
+
+    if (inStreamBuffer[ZERO] == SOH)
+    {
+        if ((reinterpret_c_style(uint16_t, &inStreamBuffer[STREAM_BUFFER_ADDRESS_INDEX_START]) + STREAM_MIN_BUFFER_SIZE) != strlen((const char*)inStreamBuffer))
+            onReceive(NSD(), (const char*)inStreamBuffer);
+            return;
+
+        storeData({inStreamBuffer[STREAM_BUFFER_SIZE_INDEX_START], &inStreamBuffer[STREAM_BUFFER_DATA_INDEX_START], reinterpret_c_style(uint16_t, &inStreamBuffer[STREAM_BUFFER_ADDRESS_INDEX_START])});
+        if (onReceive != NULL)
+            onReceive({inStreamBuffer[STREAM_BUFFER_SIZE_INDEX_START], &inStreamBuffer[STREAM_BUFFER_DATA_INDEX_START], reinterpret_c_style(uint16_t, &inStreamBuffer[STREAM_BUFFER_ADDRESS_INDEX_START])}, (const char*)inStreamBuffer);
+    }
+    else
+    {
+        onReceive(NSD(), (const char*)inStreamBuffer);
+    }
+    memset(inStreamBuffer, NULL, IN_STREAM_BUFFER_LENGTH);
+}
+
+NSD NSerialComClass::get(uint16_t addr)
+{
+    uint8_t index = search(addr);
+    if (index < NSD_LENGTH)
+        return data[index];
+    return NSD();
+}
+
+void NSerialComClass::send(NSD &newData)
+{
+    if (!newData.length)
+        return;
+    const uint8_t bufferLength = newData.length + STREAM_MIN_BUFFER_SIZE - 1;
+    uint8_t sendBuffer[bufferLength];
+    memset(sendBuffer, '_', bufferLength);
+
+    sendBuffer[ZERO] = SOH;
+    sendBuffer[STREAM_BUFFER_SIZE_INDEX_START] = newData.length;
+    uint8_t address[2] = { 122 , 122 };
+    memcpy(&sendBuffer[STREAM_BUFFER_ADDRESS_INDEX_START], &newData.address, sizeof(uint16_t));
+
+    for (uint8_t i = ZERO; i <= newData.length; i++)
+    {
+        sendBuffer[STREAM_BUFFER_DATA_INDEX_START + i] = newData.data[i];
+    }
+
+    sendBuffer[bufferLength - 1] = EOT;
+    Serial.print((const char*)sendBuffer);
+}
+
+NSerialComClass NSerial = NSerialComClass();
+
+extern void serialEvent()
+{
+    NSerial.serialEvent();
+}
--- src/NSerialCom.h
+++ src/NSerialCom.h
@@ -8,6 +8,61 @@
 #endif
 
 #include <NDefs.h>
-#include <NFuncs.h>
 
+#ifndef NSD_LENGTH
+#define NSD_LENGTH 32
+#endif
+
+#define SOH 0x01
+#define EOT 0x04
+
+#ifndef IN_STREAM_BUFFER_LENGTH
+#define IN_STREAM_BUFFER_LENGTH 38
+#endif
+
+#define STREAM_MIN_BUFFER_SIZE 6
+#define STREAM_BUFFER_SIZE_INDEX_START 1
+#define STREAM_BUFFER_ADDRESS_INDEX_START 2
+#define STREAM_BUFFER_ADDRESS_INDEX_END 3
+#define STREAM_BUFFER_DATA_INDEX_START 4
+
+#define INVALID_NSD(nsd) (nsd.length == ZERO) ? true : false
+
+struct NSerialData
+{
+    uint16_t address;
+    uint8_t *data;
+    uint8_t length;
+    NSerialData();
+    NSerialData(uint16_t, void *, uint8_t);
+    NSerialData(uint16_t, String);
+    NSerialData(uint16_t, uint32_t);
+    NSerialData(uint16_t, char *);
+    void get(uint32_t &);
+    void get(String &);
+};
+
+typedef NSerialData NSD;
+typedef NSD *pNSD;
+typedef NSD &rNSD;
+
+class NSerialComClass
+{
+private:
+    NSD data[NSD_LENGTH];
+    uint8_t nextAdd;
+    uint16_t length;
+    uint8_t search(uint16_t);
+    void storeData(NSD);
+    uint8_t inStreamBuffer[IN_STREAM_BUFFER_LENGTH] = { ZERO };
+public:
+    NSerialComClass();
+    void serialEvent();
+    NSD get(uint16_t);
+    void send(NSD &);
+    void (*onReceive)(NSD, const char*);
+};
+
+extern void serialEvent();
+extern NSerialComClass NSerial;
 #endif
