3728fe53f74db08ce63c7731ecc67bfbe173f8a3
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-05-14 02:45:30 -0400

    [ADD] Init

---
+++ src/NSerialCom.h
@@ -0,0 +1,13 @@
+#ifndef NSerialCom
+#define NSerialCom
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <NDefs.h>
+#include <NFuncs.h>
+
+#endif
